64afbb0919a7440f6fb0073571a4efeb1b6e7f44
Daniele-Angelini <60974954+Daniele-Angelini@users.noreply.github.com>
2026-07-19 00:31:34 +0200

    Update lecture schedule and locations

--- courses/mathematical-methods-for-finance-calendar.js
+++ courses/mathematical-methods-for-finance-calendar.js
@@ -24,24 +24,174 @@
  */
 
 const courseLessonEvents = [
-    /*
     {
         title: "Lecture 1",
-        start: "2025-10-06T14:00:00",
-        end: "2025-10-06T16:00:00",
+        start: "2025-10-15T10:30:00",
+        end: "2025-10-15T13:30:00",
         extendedProps: {
-            location: "Room A"
-        }
-    },
-    {
-        title: "Lecture 2",
-        start: "2025-10-08T14:00:00",
-        end: "2025-10-08T16:00:00",
-        extendedProps: {
-            location: "Room A"
+            location: "---"
         }
     }
-    */
+    {
+        title: "Lecture 2",
+        start: "2025-10-21T10:30:00",
+        end: "2025-10-21T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 3",
+        start: "2025-10-22T10:30:00",
+        end: "2025-10-22T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 4",
+        start: "2025-10-28T10:30:00",
+        end: "2025-10-28T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 5",
+        start: "2025-10-29T10:30:00",
+        end: "2025-10-29T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 6",
+        start: "2025-11-11T10:30:00",
+        end: "2025-11-11T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 7",
+        start: "2025-11-12T10:30:00",
+        end: "2025-11-12T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 8",
+        start: "2025-11-18T10:30:00",
+        end: "2025-11-18T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 9",
+        start: "2025-11-19T10:30:00",
+        end: "2025-11-19T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 10",
+        start: "2025-11-25T10:30:00",
+        end: "2025-11-25T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 11",
+        start: "2025-11-26T10:30:00",
+        end: "2025-11-26T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 12",
+        start: "2025-11-28T10:30:00",
+        end: "2025-11-28T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 13",
+        start: "2025-12-02T10:30:00",
+        end: "2025-12-02T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 14",
+        start: "2025-12-03T10:30:00",
+        end: "2025-12-03T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 15",
+        start: "2025-12-05T10:30:00",
+        end: "2025-12-05T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 16",
+        start: "2025-12-09T10:30:00",
+        end: "2025-12-09T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 17",
+        start: "2025-12-10T10:30:00",
+        end: "2025-12-10T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 18",
+        start: "2025-12-12T10:30:00",
+        end: "2025-12-12T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 19",
+        start: "2025-12-16T10:30:00",
+        end: "2025-12-16T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 20",
+        start: "2025-12-17T10:30:00",
+        end: "2025-12-17T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
+    {
+        title: "Lecture 21",
+        start: "2025-12-19T10:30:00",
+        end: "2025-12-19T13:30:00",
+        extendedProps: {
+            location: "---"
+        }
+    }
 ];
 
 /*
